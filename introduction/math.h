ac5f17b426a7e9f72e76926d0299695b2997aba0
Marcel Jar <marcel.jar@senecacollege.ca>
2025-11-26 16:01:55 -0500

    Added pragma instruction to introducion

--- introduction/math.h
+++ introduction/math.h
@@ -1,6 +1,9 @@
 #ifndef MATH_H
 #define MATH_H
 
+//#pragma once 
+// almost always work, but it is not standard-compliant
+
 int add(int a, int b);   // function prototype
 void print_message();    // another function prototype
 
