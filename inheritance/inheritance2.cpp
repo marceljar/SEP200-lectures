82588371c289196b98670746638c5616a2db93d0
Marcel Jar <marcel.jar@senecacollege.ca>
2026-01-10 22:41:56 -0500

    Added inheritance source code

---
+++ inheritance/inheritance2.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+#include <string>
+
+class Automobile {
+protected:
+    std::string brand_;
+    int maxSpeed_;
+
+public:
+    Automobile(const std::string& b, int speed)
+        : brand_(b), maxSpeed_(speed) {}
+
+    void displayInfo() const {
+        std::cout << "Brand: " << brand_
+                  << ", Max Speed: " << maxSpeed_ << " km/h\n";
+    }
+};
+
+
+class Car : public Automobile {
+private:
+    int numberOfDoors_;
+
+public:
+    Car(const std::string& b, int speed, int doors)
+        : Automobile(b, speed), numberOfDoors_(doors) {}
+
+    // shadowing the base displayInfo() function
+    void displayInfo() const {
+        Automobile::displayInfo();
+        std::cout << "Doors: " << numberOfDoors_ << std::endl;
+    }
+};
+
+int main() {
+    Car myCar("Toyota", 180, 4);    
+    myCar.displayInfo();
+    
+    return 0;
+}
---
+++ inheritance/inheritance4.cpp
@@ -0,0 +1,57 @@
+
+#include <iostream>
+#include <string>
+
+class Automobile {
+protected:
+    std::string brand_;
+    int maxSpeed_;
+
+public:
+    Automobile(const std::string& b, int speed)
+        : brand_(b), maxSpeed_(speed) {
+            std::cout << "Base constructor called" << std::endl;
+        }
+    
+    ~Automobile(){
+        std::cout << "Base destructor called" << std::endl;
+    }
+
+    // set as pure virtual, making the whole class abstract
+    virtual void displayInfo() const = 0;
+};
+
+// declared outside because it is pure virtual now
+void Automobile::displayInfo() const {
+    std::cout << "Brand: " << brand_
+                << ", Max Speed: " << maxSpeed_ << " km/h\n";
+}
+
+
+class Car : public Automobile {
+private:
+    int numberOfDoors_;
+
+public:
+    Car(const std::string& b, int speed, int doors)
+        : Automobile(b, speed), numberOfDoors_(doors) {
+           std::cout << "Derived constructor called" << std::endl;
+        }
+    
+    ~Car(){
+        std::cout << "Derived destructor called" << std::endl;
+    }
+
+    // shadowing the base displayInfo() function
+    void displayInfo() const override {
+        Automobile::displayInfo();
+        std::cout << "Doors: " << numberOfDoors_ << std::endl;
+    }
+};
+
+int main() {
+    Car myCar("Toyota", 180, 4);    
+    myCar.displayInfo();
+    
+    return 0;
+}
