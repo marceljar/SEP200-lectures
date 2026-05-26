c4257f0bcd0f8143b4a8cb2179761df0af3a856a
Marcel Jar <marcel.jar@senecacollege.ca>
2026-05-25 23:10:16 -0400

    Updated overloading source code

--- overloading/vector1.cpp
+++ overloading/vector1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <cmath>
 
-class Vector {
+class Vector
+{
 private:
     double x_;
     double y_;
@@ -9,25 +11,30 @@ public:
     Vector() : x_(0.0), y_(0.0) {}
     Vector(double x, double y) : x_(x), y_(y) {}
 
-    Vector operator+(const Vector& other) const {
+    Vector operator+(const Vector &other) const
+    {
         return Vector(x_ + other.x_, y_ + other.y_);
     }
 
-    void print() const {
-        std::cout << "(" << x_ << ", " 
+    void print() const
+    {
+        std::cout << "(" << x_ << ", "
                   << y_ << ")" << std::endl;
     }
 };
 
-int main() {
+int main()
+{
     Vector a(3.0, 2.5);
     Vector b(1.5, 2.0);
 
     Vector c = a + b;
     Vector d = a + b + b;
 
-    std::cout << "c = "; c.print();
-    std::cout << "d = "; d.print();
+    std::cout << "c = ";
+    c.print();
+    std::cout << "d = ";
+    d.print();
 
     return 0;
 }
